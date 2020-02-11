fe366e622a2ece06c1e70d5f9bf87da3d7e03de7
jwaldrip <jason@waldrip.net>
2020-02-10 22:58:38 -0700

    server compiles

--- src/routes.cr
+++ src/routes.cr
@@ -1,9 +1,6 @@
 router Server do
   static path: "/assets", dir: "./src/assets"
   root to: "home#home"
-  get "/github", format: "json" do |c|
-    c.response.puts Github.fetch_repos(Github::REST::ShardSearch, page: 1).to_json
-  end
   resources :shards
 end
 
