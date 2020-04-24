1bf12668b7872d53628548773dd6da6513f5a7f4
jwaldrip <jason@waldrip.net>
2020-04-24 14:11:32 -0600

    the robot has useful information

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def home
+    project_count = Project.query.count
+    shard_count = Shard.query.distinct("project_id").count
+    author_count = Author.query.count
+    version_count = Shard.query.count
+    dependency_count = ShardDependency.query.count
     render view: "home.slang"
   end
 end
--- src/db/setup.cr
+++ src/db/setup.cr
@@ -1,2 +1,3 @@
 ENV["DATABASE_URL"] ||= "postgres://postgres@localhost/crystalshards"
+Clear.logger.level = ::Logger::DEBUG
 Clear::SQL.init(ENV["DATABASE_URL"], connection_pool_size: 5)
