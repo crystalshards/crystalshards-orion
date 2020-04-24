5cb0a9e0198baabf02565cdb483956500c7d1e20
jwaldrip <jason@waldrip.net>
2020-04-23 21:28:33 -0600

    update routes

--- src/controllers/shards_controller.cr
+++ src/controllers/shards_controller.cr
@@ -10,13 +10,4 @@ class ShardsController < ApplicationController
 
   def show
   end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def delete
-  end
 end
--- src/routes.cr
+++ src/routes.cr
@@ -1,7 +1,8 @@
 router CrystalShards do
+  use HTTP::LogHandler.new
   static path: "/assets", dir: "./src/assets"
   root to: "home#home"
-  resources :shards
+  resources :shards, only: [:show, :index, :new, :create]
 end
 
 require "./controllers/*"
