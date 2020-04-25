4b5c6cd8ee7303269be8de09dfb6d5510d00f1e3
jwaldrip <jason@waldrip.net>
2020-04-25 14:40:47 -0600

    add recently updated

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -2,11 +2,16 @@ class HomeController < ApplicationController
   @hero_text = "Under Construction"
 
   def home
+    recently_updated = Shard.query.inner_join("projects") { projects.id == shards.project_id }.distinct("project_id").order_by("project_id": "ASC", "projects.pushed_at": "DESC").limit(10)
+    render view: "home.slang"
+  end
+
+  def stats
     project_count = Project.query.count
     shard_count = Shard.query.distinct("project_id").count
     author_count = Author.query.count
     version_count = Shard.query.count
     dependency_count = ShardDependency.query.count
-    render view: "home.slang"
+    render view: "stats.slang"
   end
 end
--- src/models/project.cr
+++ src/models/project.cr
@@ -32,6 +32,20 @@ class Project
              end
   end
 
+  def last_pushed_at_string : String
+    return "Unknown" unless (pushed_at = self.pushed_at)
+    span = Time.utc - pushed_at
+    if span.total_days >= 1
+      "#{span.total_days.to_i}d ago"
+    elsif span.total_hours >= 1
+      "#{span.total_hours.to_i}h ago"
+    elsif span.total_minutes >= 1
+      "#{span.total_minutes.to_i}m ago"
+    else
+      "#{span.total_seconds.to_i}s ago"
+    end
+  end
+
   def clone_url
     case provider
     when "github", "gitlab", "bitbucket"
