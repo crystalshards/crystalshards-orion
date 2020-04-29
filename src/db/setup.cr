a645b69ebdc73ca07287f1a73476aff0628b9bcf
jwaldrip <jason@waldrip.net>
2020-04-29 09:21:02 -0600

    no debug logging in release

--- src/db/setup.cr
+++ src/db/setup.cr
@@ -1,3 +1,3 @@
 ENV["DATABASE_URL"] ||= "postgres://postgres:postgres@localhost/crystalshards"
-Clear.logger.level = ::Logger::DEBUG
+{% unless flag?(:release) %}Clear.logger.level = ::Logger::DEBUG{% end %}
 Clear::SQL.init(ENV["DATABASE_URL"], connection_pool_size: 5)
