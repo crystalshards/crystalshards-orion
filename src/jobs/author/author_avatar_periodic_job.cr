7dce7d867babb8184071f211cd03f452a0b49e6f
jwaldrip <jason@waldrip.net>
2020-05-03 10:45:42 -0600

    try to get avatars more frequently

--- src/jobs/author/author_avatar_periodic_job.cr
+++ src/jobs/author/author_avatar_periodic_job.cr
@@ -1,13 +1,13 @@
 class Job::Author::AvatarPeriodicJob < Mosquito::PeriodicJob
-  run_every 24.hours
+  run_every 1.hour
 
   def perform
-    ::Author.emails_only.order_by("avatar_url", ["DESC", "ASC"].sample, "NULLS FIRST").order_by("updated_at", ["DESC", "ASC"].sample).each_with_cursor(100) do |author|
+    ::Author.emails_only.order_by("avatar_url", "DESC", "NULLS FIRST").each_with_cursor(100) do |author|
       if (github_user = Service::Github::REST::OwnerSearch.fetch(q: "#{author.email}+in%3Aemail", per_page: 1).items.first?)
         author.avatar_url = github_user.avatar_url
         author.save
-        sleep 1
       end
+      sleep 1
     end
   end
 end
