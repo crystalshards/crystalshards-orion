7731ed97d12bff2d30c103d17d4b790463511251
jwaldrip <jason@waldrip.net>
2020-05-11 10:13:35 -0600

    select it all

--- src/jobs/github/project_url_update_periodic_job.cr
+++ src/jobs/github/project_url_update_periodic_job.cr
@@ -2,7 +2,7 @@ class Job::Github::ProjectURLUpdatePeriodicJob < Mosquito::PeriodicJob
   run_every 1.hour
 
   def perform
-    Project.query.where { (provider == "github") & (api_id == nil) }.select("uri").each_with_cursor(100) do |project|
+    Project.query.where { (provider == "github") & (api_id == nil) }.each_with_cursor(100) do |project|
       if (repo = ::Service::Github.fetch(url: project.url))
         ProjectUpdateQueuedJob.with_payload(repo).enqueue
       end
